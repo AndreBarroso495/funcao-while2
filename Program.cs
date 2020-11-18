c767f6582710a17f56086c4558a7f949da154a46
FIC <FIC@info.local>
2020-11-18 08:24:03 -0300

    projeto concluido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace funcao_while2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Exercício tabuada - while");
+
+           Console.WriteLine("Qual tabuada você quer saber");
+           int tabuada = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("Conheça a tabuada");
+           int contador = 0;
+           while (contador <= 10)
+           {
+               Console.WriteLine(tabuada * contador);
+
+               contador = contador + 1;
+           }
+        }
+    }
+}
